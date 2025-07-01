f765e85eaa5b794ba0ad7f8b7fca2cd4b4b3d55b
Roshan Kumar Panjiyara <roshanpanziarya055@gmail.com>
2025-07-01 12:45:08 +0530

    Add user signup and login with bcryptjs
    Implemented user registration and login functionality in demo.js, including input validation and password hashing with bcryptjs. Updated signup.ejs to display error messages. Added bcryptjs as a dependency in package.json.

--- routes/demo.js
+++ routes/demo.js
@@ -1,29 +1,88 @@
-const express = require('express');
+const express = require("express");
+const bcrypt = require("bcryptjs");
 
-const db = require('../data/database');
+const db = require("../data/database");
 
 const router = express.Router();
 
-router.get('/', function (req, res) {
-  res.render('welcome');
+router.get("/", function (req, res) {
+  res.render("welcome");
 });
 
-router.get('/signup', function (req, res) {
-  res.render('signup');
+router.get("/signup", function (req, res) {
+  res.render("signup");
 });
 
-router.get('/login', function (req, res) {
-  res.render('login');
+router.get("/login", function (req, res) {
+  res.render("login");
 });
 
-router.post('/signup', async function (req, res) {});
+router.post("/signup", async function (req, res) {
+  const userData = req.body;
+  const email = userData.email;
+  const confirmEmail = userData["confirm-email"]; //since - is not allowed while accessing via .(dot)
+  const password = userData.password;
 
-router.post('/login', async function (req, res) {});
+  if (!email || !confirmEmail || !password || !email.includes("@")) {
+    return res.render("signup", { errorMessage: "Invalid inputs!" });
+  }
 
-router.get('/admin', function (req, res) {
-  res.render('admin');
+  if (email !== confirmEmail) {
+    return res.render("signup", {
+      errorMessage: "Email address does not match!"
+    });
+  }
+
+  if (password.length < 6) {
+    return res.render("signup", {
+      errorMessage: "Password must be at least 6 characters long!"
+    });
+  }
+
+  const hashPassword = await bcrypt.hash(password, 12);
+  console.log(hashPassword);
+  const user = {
+    email: email,
+    password: hashPassword,
+  };
+
+  await db.getDb().collection("users").insertOne(user);
+  res.redirect("/login");
 });
 
-router.post('/logout', function (req, res) {});
+router.post("/login", async function (req, res) {
+  const userData = req.body;
+  const email = userData.email;
+  const password = userData.password;
+
+  const existingUser = await db
+    .getDb()
+    .collection("users")
+    .findOne({ email: email });
+
+  // if (!existingUser) {
+  //   console.log("User does not exist");
+  //   return res.redirect("login", { errorMessage: "User does not exist!" });
+  // }
+
+  const isPasswordCorrect = await bcrypt.compare(
+    password,
+    existingUser.password
+  );
+
+  // if (!isPasswordCorrect) {
+  //   console.log("Password Incorrect");
+  //   return res.redirect("login", { errorMessage: "Password Incorrect!" });
+  // }
+
+  console.log("User is logged in!");
+  res.redirect("/admin");
+});
+
+router.get("/admin", function (req, res) {
+  res.render("admin");
+});
+
+router.post("/logout", function (req, res) {});
 
 module.exports = router;
